51eda32147dbc84e0683cc7b63c8291f92bbba74
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-23 09:35:08 +0200

    cepheus: Inherit from common tree
    Change-Id: Icaf0e5cb7dbb8c75b81bf9008433ab51a9b34531

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_cepheus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_cepheus-user \
+    lineage_cepheus-userdebug \
+    lineage_cepheus-eng
---
+++ lineage_cepheus.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from cepheus device
+$(call inherit-product, device/xiaomi/cepheus/device.mk)
+
+PRODUCT_NAME := lineage_cepheus
+PRODUCT_DEVICE := cepheus
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 9
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="cepheus-user 11 RKQ1.200826.002 V12.5.1.0.RFAMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/cepheus/cepheus:11/RKQ1.200826.002/V12.5.1.0.RFAMIXM:user/release-keys
