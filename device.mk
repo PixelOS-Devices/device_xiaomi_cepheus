4f5ac015267d1dcc5f7476f40b2edafa31f8fb7e
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    cepheus: Migrate to Xiaomi power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- device.mk
+++ device.mk
@@ -384,7 +384,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_msmnile
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
@@ -454,6 +454,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # Telephony
