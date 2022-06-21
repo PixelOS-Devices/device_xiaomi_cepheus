725119cd72b80f942d4e1580a789e5654f086225
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-21 12:46:41 +0200

    cepheus: Reorder device.mk
    Change-Id: I03893144869f33cf1d9e09f2865046926300bed9

--- device.mk
+++ device.mk
@@ -4,30 +4,30 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+TARGET_HAS_FOD := true
+
 # Inherit from sm8150-common
 $(call inherit-product, device/xiaomi/sm8150-common/msmnile.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2340
-TARGET_SCREEN_WIDTH := 1080
-
+# Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
-
-# Audio configs
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
-# FOD
-TARGET_HAS_FOD := true
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
