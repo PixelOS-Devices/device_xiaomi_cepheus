810ad917a53f6e19f3170a29fa0b11143fe21f0e
baalgx <bal.prxgm1407@gmail.com>
2022-09-15 15:45:42 -0500

    cepheus: PixelOS bringup
    * Inherit from common.mk instead of common_full_phone.mk, according to source.

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cepheus device
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
-# Inherit some common PE stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Boot animation
--- device.mk
+++ device.mk
@@ -377,7 +377,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-aosp
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
