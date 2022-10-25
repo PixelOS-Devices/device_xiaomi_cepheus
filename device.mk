3d852fbfc89b79bbf8c2efc1135bd15cda1e23a1
SebaUbuntu <barezzisebastiano@gmail.com>
2022-10-24 21:52:20 -0500

    cepheus: Switch to common Xiaomi light AIDL

--- device.mk
+++ device.mk
@@ -284,7 +284,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.cepheus
+    android.hardware.light-service.xiaomi
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
