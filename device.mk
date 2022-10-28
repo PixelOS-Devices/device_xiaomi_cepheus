0837c609601e61b4b77da950f50866063e3917d4
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    cepheus: Use the AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- device.mk
+++ device.mk
@@ -567,7 +567,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
