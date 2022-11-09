5ea8715ed746b20d76dbdc089939866e2756d660
Michael Bestas <mkbestas@lineageos.org>
2022-11-09 21:57:09 +0530

    cepheus: Remove android.hardware.graphics.composer@2.4-impl
    It no longer exists, the service is fully binderized
    
    Change-Id: I06ee402b50216948e0e72a2797c2edfda1c4886a

--- device.mk
+++ device.mk
@@ -128,7 +128,6 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
