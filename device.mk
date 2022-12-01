a3a7659b1c01484babf231cb6e4aa37c8b3c7888
Albert I <kras@raphielgang.org>
2022-12-01 17:10:15 +0100

    cepheus: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- device.mk
+++ device.mk
@@ -265,6 +265,10 @@ PRODUCT_PACKAGES += \
     init.affinity.sh \
     init.insmod.sh
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
