7a92253c96dc9c9cdf4d31d6ccea6515a507ebb7
baalgx <jose@pixelos.net>
2023-01-03 00:03:42 -0500

    cepheus: parts: Remove Dirac
    * To be honest it's not that good, sound is already good enough without Dirac, plus i consider it as useless bloat.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -36,7 +35,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Received boot completed intent");
 
         PendingResult pendingResult = goAsync();
-        DiracUtils.initialize(context);
         DozeUtils.onBootCompleted(context);
 
 	pendingResult.finish();
