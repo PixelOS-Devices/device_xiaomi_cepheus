b9fad0a8680dd4f308b4ffad4e6388883d020cfc
baalgx <jose@pixelos.net>
2023-01-04 23:55:59 -0500

    cepheus: Drop thermal hal * Since baalgx/kernel_xiaomi_cepheus@4867fdf * After 2 weeks of testing with/without thermal hal (1 week with, 1 without), I noticed way better temps without it. Hence, let's drop support for it.

--- device.mk
+++ device.mk
@@ -482,14 +482,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.cepheus \
-    thermal_logd
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
-
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
