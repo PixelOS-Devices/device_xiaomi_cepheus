ba4609febda414f48d8a5200b30629212a254a56
José M <jose@pixelos.net>
2023-01-08 03:52:06 -0500

    cepheus: Inherit common_full_phone makefile

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Boot animation
