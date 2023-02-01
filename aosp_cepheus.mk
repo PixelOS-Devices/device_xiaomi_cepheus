82dededd453d71aea199ecaef1c6b770a88876ee
balgxmr <jose@pixelos.net>
2023-02-01 17:16:21 -0500

    cepheus: Remove unneeded TARGET_SUPPORTS_QUICK_TAP flag

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -23,7 +23,6 @@ $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common PixelOS stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_SUPPORTS_QUICK_TAP := true
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
