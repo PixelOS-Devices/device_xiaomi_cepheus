088e1c0f41d0a5b6f7d653f8d644298e28c21dc6
balgxmr <jose@pixelos.net>
2023-02-03 01:51:29 -0500

    cepheus: Switch to OSS Mlipay

--- device.mk
+++ device.mk
@@ -343,6 +343,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
