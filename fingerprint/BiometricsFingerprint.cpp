04817bab2d97d05b4df8114892d77ed06f9330b9
balgxmr <jose@pixelos.net>
2023-02-11 22:15:54 -0500

    Revert "cepheus: fingerprint: update for new implementation"
    This reverts commit ed4a61cc8eae9298efd6aac6ebfd87aedcb14753.

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -247,7 +247,7 @@ Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69
 
 Return<RequestStatus> BiometricsFingerprint::postEnroll() {
     onFingerUp();
-    return ErrorFilter(mDevice->cancel(mDevice));
+    return ErrorFilter(mDevice->post_enroll(mDevice));
 }
 
 Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
