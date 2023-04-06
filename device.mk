e9d121e8c0cdf0affdf0285f2f086d0fadabbf18
balgxmr <jose@pixelos.net>
2023-04-06 18:41:17 -0500

    cepheus: Refactor audio configuration copying to use find-copy-subdir-files
    * Ref: https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_sm8250-common/commit/aa475ee1a706da3ccf64d21f0151596b8e9b8df6#diff-a9b707be450acdc01d52515e6b84fb8eed8a8cfb468e684a7128271ae6d5c22bR163
    
    Signed-off-by: balgxmr <jose@pixelos.net>

--- device.mk
+++ device.mk
@@ -70,10 +70,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/usbv2_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usbv2_audio_policy_configuration.xml
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
