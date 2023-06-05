080c3e26dfc0042aec365c84f7eb17eb537cae60
balgxmr <jose@pixelos.net>
2023-06-05 16:32:44 -0500

    cepheus: parts: Remove Doze
    - I'll be switching to proper xiaomi sensor impl.
    
    Signed-off-by: balgxmr <jose@pixelos.net>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,8 +23,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
-
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
@@ -33,10 +31,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
-
         PendingResult pendingResult = goAsync();
-        DozeUtils.onBootCompleted(context);
-
-	pendingResult.finish();
+	    pendingResult.finish();
     }
 }
