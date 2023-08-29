ebdb1c8b972afee3e9c51c28aa80686ea9ed65a2
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    cepheus: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -34,6 +34,19 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/xiaomi/cepheus",
+		"hardware/qcom-caf/sm8150",
+		"hardware/qcom-caf/wlan",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/commonsys/display",
+    		"vendor/qcom/opensource/commonsys-intf/display",
+		"vendor/qcom/opensource/dataservices",
+		"vendor/qcom/opensource/display",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
