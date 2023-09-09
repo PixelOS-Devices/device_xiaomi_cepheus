50a88f3754c65891a40a388ed389c9d20f78eb8f
chematelegram <chfondia@gmail.com>
2023-09-09 23:30:01 +0000

    cepheus: Build lineage health HAL
     * Support for Charging Control

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,10 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/charging_enabled
+
 # Media
 TARGET_USES_ION := true
 TARGET_DISABLED_UBWC := true
