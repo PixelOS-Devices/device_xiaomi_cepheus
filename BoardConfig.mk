1baa582634b5058014b9004cb1203a3d1121275f
chematelegram <chfondia@gmail.com>
2025-02-05 01:03:44 +0100

    cepheus: Move c2_manifest to manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,6 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/c2_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common//compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
