4c20c50602b0fa5eb59e5aedbffc955e3714f6f6
John Galt <johngaltfirstrun@gmail.com>
2025-02-07 11:51:45 -0500

    cepheus: CameraProvider: set saner values
    500 is WAAAAY too high on op7 series. Set max to a saner of 200.
    
    Also prior to this change, 80 was default for torch, so set 80 as
    default.

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -47,14 +47,12 @@ bool supportsSetTorchModeExt() {
 }
 
 int32_t getTorchDefaultStrengthLevelExt() {
-    return 50;
+    return 80;
 }
 
 int32_t getTorchMaxStrengthLevelExt() {
-    // In our device, both LEDs has same maximum value
-    // so get from one.
-    auto node = kTorchLedPaths[0] + "/" + TORCH_MAX_BRIGHTNESS;
-    return get(node, 0);
+    // 200 (out of 500) is a sane max brightness
+    return 200;
 }
 
 int32_t getTorchStrengthLevelExt() {
