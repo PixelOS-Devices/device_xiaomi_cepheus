728ee1353fcd967247a44bbac7ea783a343973a8
balgxmr <jose@pixelos.net>
2025-04-05 16:31:51 -0500

    Revert "cepheus: Inherit several Android Go configurations"
    This reverts commit a3a7659b1c01484babf231cb6e4aa37c8b3c7888.

--- device.mk
+++ device.mk
@@ -242,10 +242,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.power.rc
 
-# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
